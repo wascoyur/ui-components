887f90a99fac0f2ffc30bb66a8a24597a31cb23d
yuriy.vasilev <Yuriy.Va.Vasilev@ural.rt.ru>
2025-08-05 21:52:18 +0500

    clip-path

--- src/components/effects/clip-path/ClipPath.tsx
+++ src/components/effects/clip-path/ClipPath.tsx
@@ -6,8 +6,9 @@ export const ClipPath = () => {
           <div className={s.circle}></div>
           <div className={s.circle}></div>
       </div>
-      <div className={s.shape}>
-          <div className={s.circle}></div>
+      <div className={`${s.shape} ${s.inset}`}>
+          {`скругления карточки. Карточка обрезается`}
+      </div><div className={`${s.shape} ${s.complexNeckline}`}>
       </div>
 
   </div>
